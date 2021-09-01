300402f35d719df226e2051b1c014d9b4c0c343e
semisagi0 <semisagi0@gmail.com>
2021-09-01 15:56:37 +0900

    .mdから.htmlを生成するシェルスクリプトを作成

---
+++ build.sh
@@ -0,0 +1,7 @@
+for directory in md draft; do
+    for path in $directory/*.md; do
+        base=$(basename $path .md)
+        output_path=html/${base}.html
+        pandoc $path -s --self-contained -t html5 -c style.css -o $output_path --metadata pagetitle=$base
+    done
+done
